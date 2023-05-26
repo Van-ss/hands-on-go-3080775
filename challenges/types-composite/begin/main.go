0df3dca4c861cddd96f352371aab74e38985de56
Vanshika <85989897+Van-ss@users.noreply.github.com>
2023-05-26 01:00:01 +0000

    completed types-composite challenges

--- challenges/types-composite/begin/main.go
+++ challenges/types-composite/begin/main.go
@@ -1,6 +1,12 @@
 // challenges/types-composite/begin/main.go
 package main
 
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
 // define an author type with a name
 type author struct {
 	name string
@@ -16,28 +22,47 @@ type book struct {
 type library map[string][]book
 
 // define addBook to add a book to the library
-//
+func (l library) addBook (b book) {
+	l[b.author.name] = append(l[b.author.name], b)
+}
 
 // define a lookupByAuthor function to find books by an author's name
-//
+func (l library) lookupByAuthor(name string) []book {
+	return l[name]
+}
 
 func main() {
 	// create a new library
-	//
+	lib := library{}
 
 	// add 2 books to the library by the same author
-	//
+	jb := author{name: "James Baldwin"}
 
 	// add 1 book to the library by a different author
-	//
+	lib.addBook(book{
+		title: "The Fire Next Time",
+		author: jb,
+	})
 
+	lib.addBook(book{
+		title: "Go Tell It is on the Mountain",
+		author: jb,
+	})
+
+	lib.addBook(book{
+		title: "Meditations",
+		author: author{name: "Marcus Bald"},
+	})
 	// dump the library with spew
-	//
+	spew.Dump(lib)
 
 	// lookup books by known author in the library
-	//
-
+	books := lib.lookupByAuthor(jb.name)
+	spew.Dump(books)
 	// print out the first book's title and its author's name
-	//
+	if len(books) != 0 {
+		b := books[0]
+		fmt.Println(b.title, "by", b.author.name)
+	}
 
 }
